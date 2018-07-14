136f8195619fac966156c31f7973351bce74e5e9
sugeesh <sugeeshdeveloper@gmail.com>
2018-07-13 23:37:43 -0500

    added css for the buttons

--- imports/ui/Team-stats.jsx
+++ imports/ui/Team-stats.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import {Radar} from 'react-chartjs-2';
 import Divider from 'material-ui/Divider';
 
+
+
 export default class TeamStats extends Component {
   render() {
     const players = this.props.players;
@@ -9,39 +11,41 @@ export default class TeamStats extends Component {
 
     const ballManipulation = Math.round((players.reduce((ballManipulation, player) => {
       return ballManipulation + player.ballManipulation;
-    }, 0) / (3 * numPlayers )) * 100 );
+    }, 0) / ( 3 * numPlayers )) * 100 );
 
     const kickingAbilities = Math.round((players.reduce((kickingAbilities, player) => {
       return kickingAbilities + player.kickingAbilities;
-    }, 0) / (3 * numPlayers )) * 100 );
+    }, 0) / ( 3 * numPlayers )) * 100 );
 
     const passingAbilities = Math.round((players.reduce((passingAbilities, player) => {
       return passingAbilities + player.passingAbilities;
-    }, 0) / (3 * numPlayers )) * 100 );
+    }, 0) / ( 3 * numPlayers )) * 100 );
 
     const duelTackling = Math.round((players.reduce((duelTackling, player) => {
       return duelTackling + player.duelTackling;
-    }, 0) / (3 * numPlayers )) * 100 );
+    }, 0) / ( 3 * numPlayers )) * 100 );
 
     const fieldCoverage = Math.round((players.reduce((fieldCoverage, player) => {
       return fieldCoverage + player.fieldCoverage;
-    }, 0) / (3 * numPlayers )) * 100 );
+    }, 0) / ( 3 * numPlayers )) * 100 );
 
     const blockingAbilities = Math.round((players.reduce((blockingAbilities, player) => {
       return blockingAbilities + player.blockingAbilities;
-    }, 0) / (3 * numPlayers )) * 100 );
+    }, 0) / ( 3 * numPlayers )) * 100 );
 
     const gameStrategy = Math.round((players.reduce((gameStrategy, player) => {
       return gameStrategy + player.gameStrategy;
-    }, 0) / (3 * numPlayers )) * 100 );
+    }, 0) / ( 3 * numPlayers )) * 100 );
 
     const playmakingRisks = Math.round((players.reduce((playmakingRisks, player) => {
       return playmakingRisks + player.playmakingRisks;
-    }, 0) / (3 * numPlayers )) * 100 );
+    }, 0) / ( 3 * numPlayers )) * 100 );
 
     const defense = Math.round((duelTackling + fieldCoverage + blockingAbilities + gameStrategy + playmakingRisks)/5);
-    const offense = Math.round((kickingAbilities + ballManipulation + passingAbilities + fieldCoverage + gameStrategy + playmakingRisks)/6);
-    const total = Math.round((kickingAbilities + ballManipulation + passingAbilities + fieldCoverage + gameStrategy + playmakingRisks + duelTackling + blockingAbilities)/8);
+    const offense = Math.round((kickingAbilities + ballManipulation +
+      passingAbilities + fieldCoverage + gameStrategy + playmakingRisks)/6);
+    const total = Math.round((kickingAbilities + ballManipulation +
+      passingAbilities + fieldCoverage + gameStrategy + playmakingRisks + duelTackling + blockingAbilities)/8);
 
     const data = {
       labels: ['Ball Manipulation', 'Kicking', 'Passing', 'Duel/Tackling', 'Field Coverage', 'Blocking', 'Strategy', 'Risks'],
@@ -50,11 +54,12 @@ export default class TeamStats extends Component {
           label: 'In % of max possible',
           backgroundColor: 'rgba(143, 202, 249, 0.2)',
           borderColor: 'rgba(12, 71, 161, 1)',
-          pointBackgroundColor: 'rgba((12, 71, 161, 1)',
+          pointBackgroundColor: 'rgba(12, 71, 161, 1)',
           pointBorderColor: '#fff',
           pointHoverBackgroundColor: '#fff',
-          pointHoverBorderColor: 'rgba((12, 71, 161, 1)',
-          data: [ballManipulation, kickingAbilities, passingAbilities, duelTackling, fieldCoverage, blockingAbilities, gameStrategy, playmakingRisks]
+          pointHoverBorderColor: 'rgba(12, 71, 161, 1)',
+          data: [ballManipulation, kickingAbilities, passingAbilities, duelTackling, fieldCoverage,
+            blockingAbilities, gameStrategy, playmakingRisks]
         }
       ]
     };
